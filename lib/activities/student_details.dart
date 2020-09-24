2d00c9bd4a708fde8fe78facb0fca71450fed6c6
Aymen Masmoudi <aymen.msd@hotmail.fr>
2020-09-24 10:53:44 +0200

    control arrow back and onBackPressed

--- lib/activities/student_details.dart
+++ lib/activities/student_details.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 class StudentDetails extends StatefulWidget{
+
   String screenTitle = "Student Details";
+
   StudentDetails(this.screenTitle);
+
   @override
   State<StatefulWidget> createState() {
     return Student(screenTitle);
@@ -27,71 +30,82 @@ class Student extends State<StudentDetails>{
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(screenTitle),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(15),
-        child: ListView(
-          children: <Widget>[
-            ListTile(
-              title: DropdownButton(
-                onChanged: (String selectedItem){
-                  setState(() {
-                    studentStatus = selectedItem;
-                  });
-                },
-                items: status.map((String item){
-                  return DropdownMenuItem<String>(
-                    value: item,
-                    child: Text(item),
-                  );
-                }).toList(),
-                value: studentStatus,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                controller: studentName,
-                decoration: textFieldDecoration("add name"),
-                onChanged: (name){
-                  this.name = name;
-                },
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                controller: studentDescription,
-                decoration: textFieldDecoration("add description"),
-                onChanged: (description){
-                  this.description = description;
-                },
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: FlatButton(
-                onPressed: (){
-                  debugPrint("try to save student");
-                },
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    side: BorderSide(width: 1, color: Colors.redAccent)
+    return WillPopScope(
+      onWillPop: (){
+        onBackPressed();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(screenTitle),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: (){
+              onBackPressed();
+            },
+          ),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(15),
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                title: DropdownButton(
+                  onChanged: (String selectedItem){
+                    setState(() {
+                      studentStatus = selectedItem;
+                    });
+                  },
+                  items: status.map((String item){
+                    return DropdownMenuItem<String>(
+                      value: item,
+                      child: Text(item),
+                    );
+                  }).toList(),
+                  value: studentStatus,
                 ),
-                child: Text(
-                  "SAVE",
-                  style: TextStyle(
-                      color: Colors.redAccent
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  controller: studentName,
+                  decoration: textFieldDecoration("add name"),
+                  onChanged: (name){
+                    this.name = name;
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  controller: studentDescription,
+                  decoration: textFieldDecoration("add description"),
+                  onChanged: (description){
+                    this.description = description;
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: FlatButton(
+                  onPressed: (){
+                    debugPrint("try to save student");
+                  },
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      side: BorderSide(width: 1, color: Colors.redAccent)
+                  ),
+                  child: Text(
+                    "SAVE",
+                    style: TextStyle(
+                        color: Colors.redAccent
+                    ),
                   ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
-      ),
+      )
     );
   }
 
@@ -125,6 +139,10 @@ class Student extends State<StudentDetails>{
       hintText: hintText,
       hintStyle: TextStyle(fontSize: 16, color: Color(0xFFB3B1B1)),
     );
+  }
+
+  void onBackPressed(){
+    Navigator.pop(context);
   }
 
 }
